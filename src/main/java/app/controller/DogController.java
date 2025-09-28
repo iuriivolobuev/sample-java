1fd049594b616c3cf6167849877a49533a231e12
Iurii Volobuev <iurii.volobuev@gmail.com>
2025-09-29 01:34:38 +0200

    wip

--- src/main/java/app/controller/DogController.java
+++ src/main/java/app/controller/DogController.java
@@ -1,6 +1,5 @@
 package app.controller;
 
-import app.dao.DogDao;
 import app.domain.ObjectNotFoundException;
 import app.dto.DogDto;
 import app.service.DogService;
@@ -19,44 +18,42 @@ import java.util.UUID;
 public class DogController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final DogService dogService;
-    private final DogDao dogDao;
 
-    public DogController(DogService dogService, DogDao dogDao) {
+    public DogController(DogService dogService) {
         this.dogService = dogService;
-        this.dogDao = dogDao;
     }
 
     @GetMapping("/dog")
     public Collection<DogDto> getAllDogs() {
         logger.info("Getting all the dogs.");
-        return DogDto.fromDomain(dogDao.getAllDogs());
+        return DogDto.fromDomain(dogService.getAllDogs());
     }
 
     @GetMapping("/dog/{id}")
     public DogDto getDog(@PathVariable String id) {
         logger.info("Getting a dog: id=[{}].", id);
-        return DogDto.fromDomain(dogDao.getDog(id));
+        return DogDto.fromDomain(dogService.getDog(id));
     }
 
     @PostMapping("/dog")
     public DogDto createDog(@RequestBody @Valid DogDto dog) {
         logger.info("Creating a dog: [{}].", dog);
         dog.setId(UUID.randomUUID().toString());
-        return DogDto.fromDomain(dogDao.createDog(dog.toDomain()));
+        return DogDto.fromDomain(dogService.createDog(dog.toDomain()));
     }
 
     @PutMapping("/dog/{id}")
     public DogDto updateDog(@PathVariable String id, @RequestBody @Valid DogDto dog) {
         logger.info("Updating a dog: id=[{}].", id);
         dog.setId(id);
-        dogDao.updateDog(dog.toDomain());
+        dogService.updateDog(dog.toDomain());
         return dog;
     }
 
     @DeleteMapping("/dog/{id}")
     public void deleteDog(@PathVariable String id) {
         logger.info("Deleting a dog: id=[{}].", id);
-        dogDao.deleteDog(id);
+        dogService.deleteDog(id);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
--- src/main/java/app/service/DogService.java
+++ src/main/java/app/service/DogService.java
@@ -1,4 +1,17 @@
 package app.service;
 
-public class DogService {
+import app.domain.Dog;
+
+import java.util.Collection;
+
+public interface DogService {
+    Collection<Dog> getAllDogs();
+
+    Dog getDog(String id);
+
+    Dog createDog(Dog dog);
+
+    Dog updateDog(Dog dog);
+
+    boolean deleteDog(String id);
 }
