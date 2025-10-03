67f1a7f37abebe2b5e63739fe5bb0f77209eda22
Iurii Volobuev <iurii.volobuev@gmail.com>
2025-10-03 14:00:08 +0200

    with postgres

--- src/main/resources/migrations/V001__init.sql
+++ src/main/resources/migrations/V001__init.sql
@@ -1,7 +1,7 @@
 create table DOG (
   ID varchar(36) primary key,
-  NAME nvarchar(1000) not null,
+  NAME varchar(1000) not null,
   TIME_OF_BIRTH timestamp,
-  HEIGHT double,
-  WEIGHT double
+  HEIGHT double precision,
+  WEIGHT double precision
 );
