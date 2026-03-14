9410b93387ac2a1f2c1a59afccffb4967a6808ac
Iurii Volobuev <iurii.volobuev@gmail.com>
2026-03-14 17:42:27 +0100

    Rename to web-context.xml

--- src/test/java/app/controller/MockMvcTest.java
+++ src/test/java/app/controller/MockMvcTest.java
@@ -9,18 +9,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * An annotation that you need to put on Tests that simulate HTTP requests. Instead of actually sending an HTTP request
+ * An annotation that you need to put on tests that simulate HTTP requests. Instead of actually sending an HTTP request
  * (which is slow) it works with Spring MVC directly to invoke the methods of the controllers/endpoints.
- * <p>
- * Annotations is a much better way to share common configuration than extending a common test class:
+ *
+ * <p>Annotations is a much better way to share common configuration than extending a common test class:
  * <ul>
- *     <li>This makes it easier to read the code (it's pretty hard to find things out when you have a whole hierarchy
+ *     <li>this makes it easier to read the code (it's pretty hard to find things out when you have a whole hierarchy
  * of classes)</li>
- *     <li>It's more flexible since you can set multiple annotations on the same test class</li>
+ *     <li>it's more flexible since you can set multiple annotations on the same test class</li>
  * </ul>
- * </p>
  */
 @WebAppConfiguration
-@ContextConfiguration({"classpath:/test-context.xml", "classpath:/web-app-context.xml"})
+@ContextConfiguration({"classpath:/test-context.xml", "classpath:/web-context.xml"})
 @Target(ElementType.TYPE) @Retention(RetentionPolicy.RUNTIME)
 public @interface MockMvcTest {}
